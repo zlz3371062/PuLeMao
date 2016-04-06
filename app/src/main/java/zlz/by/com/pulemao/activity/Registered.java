916c1a588ffa006c1f1dff4d1885ee44a946c677
zlz <438580470@qq.com>
2016-04-06 15:53:02 +0800

    第二次提交增加注册页面

--- app/src/main/java/zlz/by/com/pulemao/activity/Registered.java
+++ app/src/main/java/zlz/by/com/pulemao/activity/Registered.java
@@ -9,6 +9,7 @@ import zlz.by.com.pulemao.util.BaseActivity;
 
 public class Registered extends BaseActivity {
     private TextView loginbtnback;
+    private TextView registeredtitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +20,10 @@ public class Registered extends BaseActivity {
     private void init() {
         loginbtnback = (TextView) findViewById(R.id.headtitlelefttxt);
         loginbtnback.setVisibility(View.INVISIBLE);
+        registeredtitle= (TextView) findViewById(R.id.headapptitle);
+        registeredtitle.setText("注册");
     }
-
+//    tvTest.getPaint().setFlags(Paint. UNDERLINE_TEXT_FLAG ); //下划线
+//    tvTest.getPaint().setAntiAlias(true);//抗锯齿
 
 }
