772839be21fec77fa9a5d27ae3b7a30da41b8139
zlz <438580470@qq.com>
2016-04-06 15:34:50 +0800

    第二次提交增加注册页面

---
+++ app/src/main/java/zlz/by/com/pulemao/activity/Registered.java
@@ -0,0 +1,25 @@
+package zlz.by.com.pulemao.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import zlz.by.com.pulemao.R;
+import zlz.by.com.pulemao.util.BaseActivity;
+
+public class Registered extends BaseActivity {
+    private TextView loginbtnback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registered);
+        init();
+    }
+
+    private void init() {
+        loginbtnback = (TextView) findViewById(R.id.headtitlelefttxt);
+        loginbtnback.setVisibility(View.INVISIBLE);
+    }
+
+
+}
