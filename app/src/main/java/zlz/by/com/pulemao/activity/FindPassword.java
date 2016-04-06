9b6f7b7c64c9e1f766f4adde6a71f91b8e0faa00
zlz <438580470@qq.com>
2016-04-06 16:17:25 +0800

    第二次提交增加注册页面

--- app/src/main/java/zlz/by/com/pulemao/activity/FindPassword.java
+++ app/src/main/java/zlz/by/com/pulemao/activity/FindPassword.java
@@ -1,6 +1,8 @@
 package zlz.by.com.pulemao.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import zlz.by.com.pulemao.R;
 import zlz.by.com.pulemao.util.BaseActivity;
@@ -9,10 +11,21 @@ import zlz.by.com.pulemao.util.BaseActivity;
  * Created by mac on 16/4/6.
  */
 public class FindPassword extends BaseActivity {
+     private TextView apptitle,applefttxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.findpassword);
+        init();
+
+    }
+
+    private void init() {
+        apptitle = (TextView) findViewById(R.id.headapptitle);
+        applefttxt = (TextView) findViewById(R.id.headtitlelefttxt);
+        apptitle.setText("找回密码");
+        applefttxt.setVisibility(View.INVISIBLE);
     }
 }
