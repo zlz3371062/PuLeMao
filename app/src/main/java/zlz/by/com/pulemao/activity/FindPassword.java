4cb68829e5f2b89764238c4138756852e2b8d403
zlz <438580470@qq.com>
2016-04-06 16:03:30 +0800

    第二次提交增加注册页面

---
+++ app/src/main/java/zlz/by/com/pulemao/activity/FindPassword.java
@@ -0,0 +1,18 @@
+package zlz.by.com.pulemao.activity;
+
+import android.os.Bundle;
+
+import zlz.by.com.pulemao.R;
+import zlz.by.com.pulemao.util.BaseActivity;
+
+/**
+ * Created by mac on 16/4/6.
+ */
+public class FindPassword extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.findpassword);
+    }
+}
