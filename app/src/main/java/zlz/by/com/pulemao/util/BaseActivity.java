d428466b301fc4d38fe25170f0ff0f89d7af9ccc
zlz <438580470@qq.com>
2016-04-07 11:59:32 +0800

    第二次提交增加注册页面

--- app/src/main/java/zlz/by/com/pulemao/util/BaseActivity.java
+++ app/src/main/java/zlz/by/com/pulemao/util/BaseActivity.java
@@ -1,20 +1,34 @@
 package zlz.by.com.pulemao.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import zlz.by.com.pulemao.R;
+import zlz.by.com.pulemao.model.Screen;
 
 /**
  * Created by mac on 16/4/6.
  */
 public class BaseActivity extends Activity{
+
+    private ImageView apptitlebackimg;
+    private LinearLayout apptitleleftimgll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //得到屏幕长款px的方法一：
+        WindowManager wn = this.getWindowManager();
+        Screen.ScreenHeight = wn.getDefaultDisplay().getHeight();
+        Screen.ScreenWidth = wn.getDefaultDisplay().getWidth();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             setTranslucentStatus(true);
             SystemBarTintManager tintManager = new SystemBarTintManager(this);
@@ -23,6 +37,18 @@ public class BaseActivity extends Activity{
         }
 
     }
+
+    public void apptitleset(Context con) {
+        LayoutInflater apptitlein =  LayoutInflater.from(con);
+        View view  = apptitlein.inflate(R.layout.head,null);
+        apptitleleftimgll = (LinearLayout) findViewById(R.id.apptitleleftimgll);
+
+        apptitlebackimg = new ImageView(BaseActivity.this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(Screen.ScreenWidth/25, Screen.ScreenHeight/30);
+        apptitlebackimg.setImageResource(R.drawable.nav_arrow);
+        apptitleleftimgll.addView(apptitlebackimg, layoutParams);
+    }
+
     private void setTranslucentStatus(boolean on) {
         Window win = getWindow();
         WindowManager.LayoutParams winParams = win.getAttributes();
